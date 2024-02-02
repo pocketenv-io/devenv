537b4220112fe401a4d0907baedc1e1425568b16
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-02 20:31:29 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Devenv Workspace
+
+This is a template workspace with devenv. It is a starting point for creating a new workspace.
+
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "hostname" {
+  type = string
+  description = "Hostname of the container"
+  default = null
+} 
+
+variable "image" {
+  type = string
+  description = "Image to use, required if context is not set"
+  default = "ghcr.io/pocketenv-io/devenv:main"
+}
+
+variable "workspace_name" {
+  type = string
+  description = "Name of the workspace"
+  default = "devenv-workspace"
+}
+
+variable "user" {
+  type = string
+  description = "User to create"
+  default = "coder"
+}
+
+variable "context" {
+  type = string
+  description = "Context to use"
+  default = null
+}
+
+variable "volumes" {
+  type = map(string)
+  description = "Volumes to mount"
+  default = {
+    devenv-workspace = "/home/coder"
+    devenv-nix = "/nix"
+  }
+}
